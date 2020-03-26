d0ea24f4cfbed7f75406e5992fca82d5b75dc9d7
Stefan-Stev <60571585+Stefan-Stev@users.noreply.github.com>
2020-03-26 16:08:44 +0200

    Add files via upload

---
+++ lab6/DrawingPanel.java
@@ -0,0 +1,102 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+import java.lang.Object;
+import java.awt.Color;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author stefan
+ */
+public class DrawingPanel extends javax.swing.JPanel {
+
+   
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+
+    final MainFrame frame;
+    final static int W=800, H=600;
+    BufferedImage image;
+    Graphics2D graphics;
+    public DrawingPanel (MainFrame frame){
+        this.frame=frame;
+        init();
+    }
+    private void createOffscreenImage()
+    {
+        image=new BufferedImage(W,H,BufferedImage.TYPE_INT_ARGB);
+        graphics=image.createGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, W, H);
+    }          
+    private void init() {
+        setPreferredSize(new Dimension(W, H)); 
+        setBorder(BorderFactory.createEtchedBorder()); 
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                drawShape(e.getX(), e.getY());
+                repaint();
+            } 
+        });
+    }
+    private void drawShape(int x,int y){
+        Random rand=new Random();
+                int radius=rand.nextInt(90);
+                int sides=(int) frame.configPanel.sidesField.getValue();
+                Color color ;
+                if(frame.configPanel.colorCombo.getSelectedItem()=="Random")
+                {
+                    int r=rand.nextInt(255-128)+128;
+                    int g=rand.nextInt(255-128)+128;
+                    int b=rand.nextInt(255-128)+128;
+                    //cica culorile transparente sunt intre 128 si 255
+                   color=new Color(r,g,b); //am creat  culoare 
+                }else
+                {
+                    color=Color.BLACK;
+                }
+                graphics.setColor(color);
+                graphics.fill(new RegularPolygon(x, y, radius, sides));
+                
+    }
+
+  @Override
+    public void update(Graphics g) {
+    } //Why did I do that?
+ @Override
+    protected void paintComponent(Graphics g) {
+        g.drawImage(image, 0, 0, this);
+    }
+    
+  
+
+}
+
+
---
+++ lab6/NodeShape.java
@@ -0,0 +1,20 @@
+
+import java.awt.geom.Ellipse2D;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author stefan
+ */
+public class NodeShape extends Ellipse2D.Double {
+    public NodeShape(double x0,double y0,double radius)
+    {
+        super(x0-radius/2,y0-radius/2,radius,radius);
+        
+    }
+}
