848193e633926c4a11e041e4d92553a5990848be
Stefan-Stev <60571585+Stefan-Stev@users.noreply.github.com>
2020-04-09 17:02:22 +0300

    Update Main.java

--- lab8/Main.java
+++ lab8/Main.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-        Connection con = Database.getConn("STUDENT","STUDENT");
+        Connection con = Database.getConn("root","");
 
         ArtistController artist = new ArtistController();
         AlbumController album = new AlbumController();
